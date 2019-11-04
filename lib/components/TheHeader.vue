0f26969809edd15fe7838a24e27c78d4fd1a87ec
yuchanns <airamusume@gmail.com>
2019-11-04 22:32:48 +0800

    add components Hamburguer&&Logo

--- lib/components/TheHeader.vue
+++ lib/components/TheHeader.vue
@@ -5,23 +5,12 @@
     class="header">
     <div class="row header-top">
       <div class="column xs-33 sm-20">
-        <div class="hamburguer header-top__hamburguer">
-          <div class="hamburguer__wrapper">
-            <span class="hamburguer__line"></span>
-            <span class="hamburguer__line"></span>
-            <span class="hamburguer__line"></span>
-          </div>
-        </div>
+        <menu-hamburguer
+          class="header-top__hamburguer" />
       </div>
       <div class="column xs-33 sm-60">
         <h1 class="header__logo">
-          <router-link to="/" class="header-top-logo__link">
-            <img itemprop="logo"
-              :src="$site.themeConfig.logo"
-              height="47px"
-              width="50px"
-              alt="$site.title">
-          </router-link>
+          <logo class="header-top-logo__link" />
         </h1>
       </div>
       <div class="column xs-33 sm-20">
@@ -36,8 +25,16 @@
 </template>
 
 <script>
+import MenuHamburguer from '@theme/components/Hamburguer'
+import Logo from '@theme/components/Logo'
+
 export default {
-  name: 'TheHeader'
+  name: 'TheHeader',
+
+  components: {
+    MenuHamburguer,
+    Logo
+  }
 }
 </script>
 
@@ -59,38 +56,6 @@ export default {
 
   &__logo
     text-align center
-
-.hamburguer
-  width 44px
-  height 44px
-  cursor pointer
-  &__wrapper
-    width 30px
-    position relative
-    left 50%
-    top 12px
-    margin-left -15px
-    transition .5s ease-in-out
-    transform rotate(0deg)
-  &__line
-    display block
-    position absolute
-    height 5px
-    width 100%
-    background-color $primaryColor
-    border-radius 50px
-    opacity 1
-    left 0
-    transition .25s ease-in-out
-    transform rotate(0deg)
-    &:first-child
-      top 0
-    &:nth-child(2)
-      top 8px
-      width 60%
-    &:nth-child(3)
-      top 16px
-      width 80%
 
 .search
 .search__icon
