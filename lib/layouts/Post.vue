022c4176ade599d164eea848d5b14ca35c497f53
yuchanns <airamusume@gmail.com>
2020-01-29 22:34:17 +0800

    Fix:lint

--- lib/layouts/Post.vue
+++ lib/layouts/Post.vue
@@ -47,8 +47,6 @@ import BackIcon from '@theme/components/icons/BackIcon'
 import FixedHead from '@theme/components/FixedHead'
 import { getDistanceToNow, getDistanceToNowUnixNano } from '@theme/utils/compare-time'
 import { sortPosts } from '@theme/utils/sort-posts'
-import { parseISO } from 'date-fns'
-import compareDesc from 'date-fns/compareDesc'
 import _ from 'lodash'
 
 export default {
